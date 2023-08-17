c744066b3d5344c6c9a26109b6c7c67f3c62a890
Francis Williams <franciswilliams1999@gmail.com>
2023-08-16 22:17:26 -0500

    minor error fix in Registration form

--- ui/src/pages/Register/RegistrationForm.jsx
+++ ui/src/pages/Register/RegistrationForm.jsx
@@ -2,11 +2,14 @@ import { useContext, useEffect, useReducer, useState } from "react";
 import { useNavigate } from 'react-router-dom'
 import { Typography } from "@mui/material"; // Using Material-UI components
 import { ApiController } from "../../utils/api";
+import { RequestReducer } from "../../utils/requestReducer"
 import AuthContext from "../../context/AuthContext/AuthContext";
 import InputForm from "../../components/InputForm";
 
 const RegistrationForm = () => {
    const { user, login } = useContext(AuthContext)
+
+   const [createUserState, createUserDispath] = useReducer(RequestReducer.reducer, RequestReducer.defaultState)
 
   useEffect(() => {
     if (user) {
@@ -27,14 +30,17 @@ const RegistrationForm = () => {
   const navigate = useNavigate();
 
   const createUser = async (data) => {
+    createUserDispath(RequestReducer.setLoading(true));
     ApiController.createUser(data)
-    .then((response) =>{
+    .then((response) => {
       const newUser = {...user, username: response.username, id: response._id};
+      createUserDispath(RequestReducer.setData(newUser));
       login(newUser)
       navigate('/resume')
     } )
     .catch((error) => {
       console.log(error)
+      createUserDispath(RequestReducer.setError(error));
     })
   };
 
@@ -46,6 +52,8 @@ const RegistrationForm = () => {
         formData={formData} 
         setFormData={setFormData}
         onSubmit={createUser}
+        loading={createUserState.loading}
+        error={createUserState.error}
         disabledFields={new Set(['email'])}
         requiredFields={new Set(Object.keys(formData))}
       />
